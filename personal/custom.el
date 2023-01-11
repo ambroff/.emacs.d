0273d83f2c7ef8f0f9941e2da33d922d56bc05e0
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2023-01-11 15:06:13 -0800

    Install org-roam

--- personal/custom.el
+++ personal/custom.el
@@ -237,3 +237,11 @@
 (dir-locals-set-directory-class
  (expand-file-name "~/code/linux")
  'linux-kernel)
+
+(use-package org-roam
+  :ensure t
+  :custom (org-roam-directory "~/Notes")
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config (org-roam-setup))
