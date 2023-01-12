dab757611f3a0c9f03fe1c65329cca6ac90420a6
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2023-01-11 18:14:38 -0800

    More advanced org-roam setup

--- personal/custom.el
+++ personal/custom.el
@@ -291,10 +291,16 @@
 
 (use-package org-roam
   :ensure t
-  :custom (org-roam-directory "~/Notes")
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/Notes")
+  (org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point))
   :config (org-roam-setup))
 
 ;; Disable line numbers for some modes
